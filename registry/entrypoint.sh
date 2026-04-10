74b3ac96d9b91110be5369f570e75edc972ff3c0
sumansaurabh <ss.sumansaurabh92@gmail.com>
2026-04-10 23:15:15 +0530

    refactor: enhance entrypoint script for dynamic configuration rendering

--- registry/entrypoint.sh
+++ registry/entrypoint.sh
@@ -1,22 +1,45 @@
 #!/bin/sh
 
-set -e
+set -eu
 
-sed -i "s/__PORT__/$PORT/g" /etc/docker/registry/config.yml
-sed -i "s/__HOOK_TOKEN__/$HOOK_TOKEN/g" /etc/docker/registry/config.yml
-sed -i "s/__HOOK_URI__/$HOOK_URI/g" /etc/docker/registry/config.yml
-sed -i "s/__REPLREG_HOST__/$REPLREG_HOST/g" /etc/docker/registry/config.yml
-sed -i "s/__REPLREG_SECRET__/$REPLREG_SECRET/g" /etc/docker/registry/config.yml
+SOURCE_CONFIG="/etc/docker/registry/config.yml"
+RUNTIME_CONFIG="/tmp/registry-config.yml"
+NEEDS_RENDER="false"
 
-sed -i "s/__S3_REGION__/$S3_REGION/g" /etc/docker/registry/config.yml
-sed -i "s/__S3_BUCKET__/$S3_BUCKET/g" /etc/docker/registry/config.yml
-sed -i "s/__S3_ENDPOINT__/$S3_ENDPOINT/g" /etc/docker/registry/config.yml
+escape_sed_replacement() {
+    printf '%s' "$1" | sed 's/[&|\\]/\\&/g'
+}
+
+if [ "$#" -gt 0 ]; then
+    case "$1" in
+        *.yaml|*.yml)
+            SOURCE_CONFIG="$1"
+            NEEDS_RENDER="true"
+            ;;
+    esac
+else
+    set -- "$SOURCE_CONFIG"
+    NEEDS_RENDER="true"
+fi
+
+if [ "$NEEDS_RENDER" = "true" ]; then
+    sed \
+        -e "s|__PORT__|$(escape_sed_replacement "${PORT}")|g" \
+        -e "s|__HOOK_TOKEN__|$(escape_sed_replacement "${HOOK_TOKEN}")|g" \
+        -e "s|__HOOK_URI__|$(escape_sed_replacement "${HOOK_URI}")|g" \
+        -e "s|__REPLREG_HOST__|$(escape_sed_replacement "${REPLREG_HOST}")|g" \
+        -e "s|__REPLREG_SECRET__|$(escape_sed_replacement "${REPLREG_SECRET}")|g" \
+        -e "s|__S3_REGION__|$(escape_sed_replacement "${S3_REGION}")|g" \
+        -e "s|__S3_BUCKET__|$(escape_sed_replacement "${S3_BUCKET}")|g" \
+        -e "s|__S3_ENDPOINT__|$(escape_sed_replacement "${S3_ENDPOINT}")|g" \
+        "$SOURCE_CONFIG" > "$RUNTIME_CONFIG"
+fi
 
 # Run garbage collection job in background
 # /garbage-collect.sh &
 
-case "$1" in
-    *.yaml|*.yml) set -- registry serve "$@" ;;
+case "${1:-}" in
+    *.yaml|*.yml) set -- registry serve "$RUNTIME_CONFIG" ;;
     serve|garbage-collect|help|-*) set -- registry "$@" ;;
 esac
 
