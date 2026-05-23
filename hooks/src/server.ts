6f0e9c32777e34450f695c4cd85bd43639386531
sumansaurabh <ss.sumansaurabh92@gmail.com>
2026-05-23 09:25:34 +0530

    refactor: update yargs implementation to use hideBin and parse method for improved CLI handling

--- hooks/src/server.ts
+++ hooks/src/server.ts
@@ -1,11 +1,12 @@
-import * as yargs from "yargs";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
 import * as hooksCommand from "./commands/hooks";
 import * as reapCommand from "./commands/reap";
 
-yargs
+yargs(hideBin(process.argv))
   .command(hooksCommand as any)
   .command(reapCommand as any)
   .env()
   .help()
   .demandCommand()
-  .argv;
+  .parse();
